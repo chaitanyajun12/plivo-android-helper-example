cec521332d2d7447f78c59b4b295d3425d2fce48
Krishna Chaitanya M <chaitanya.jun12@gmail.com>
2013-07-22 14:11:07 +0530

    Plivo Android API example for Call

---
+++ src/com/test/plivosdk/TestPlivoActivity.java
@@ -0,0 +1,57 @@
+package com.test.plivosdk;
+
+import java.util.LinkedHashMap;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.plivo.helper.android.attributes.CallAttributes;
+import com.plivo.helper.android.exception.AndroidClientException;
+import com.plivo.helper.android.rest.HttpFactory;
+
+public class TestPlivoActivity extends Activity {
+	/** Called when the activity is first created. */
+
+	private String AUTH_ID = "";	// AUTH_ID
+	private String AUTH_TOKEN = "";	// AUTH_TOKEN
+	
+	private HttpFactory mHttp;
+	private LinkedHashMap<String, String> params;
+
+	private String FROM = "";		// <FROM>
+	private String TO = "";			// <TO>
+	private String ANSWER_URL = "";	// <ANSWER_URL>
+		
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		mHttp = new HttpFactory(AUTH_ID, AUTH_TOKEN);
+
+		params = new LinkedHashMap<String, String>();
+		params.put(CallAttributes.FROM, FROM);
+		params.put(CallAttributes.TO, TO);
+		params.put(CallAttributes.ANSWER_URL, ANSWER_URL);
+
+		Thread callThread = new Thread(callRunnable);
+		callThread.start();
+	}
+
+	Runnable callRunnable = new Runnable() {
+
+		@Override
+		public void run() {
+
+			try {
+				mHttp.makeCall(params);
+			} catch (AndroidClientException e) {
+				e.printStackTrace();
+				Log.e("plivosdk", "Error: " + e.getMessage());
+			}
+
+		}
+	};
+
+}
